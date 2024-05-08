ea9887a4aad93c3d6148eaecd96f9828ed46cabf
Sonakatyal <119148034+Sonakatyal@users.noreply.github.com>
2024-05-08 22:40:41 +0530

    Add files via upload

---
+++ react 1/IIF/script.js
@@ -0,0 +1,33 @@
+// Immediately invoked functions expressions
+
+let a  = ()=>{
+    return new Promise((resolve, reject)=>{
+       setTimeout(() => {
+           resolve(456)
+       }, 4000);
+
+} )
+}
+
+// let f = async () =>{
+
+//     let b = await a();
+//     console.log(b)
+//     let c = await a();
+//     console.log(c)
+//     let d = await a();
+//     console.log(d)
+// }
+
+// f()
+
+( async () =>{
+
+   let b = await a();
+   console.log(b)
+   let c = await a();
+   console.log(c)
+   let d = await a();
+   console.log(d)
+})()
+
---
+++ react 1/destructuring/script.js
@@ -0,0 +1,4 @@
+let arr = [3,5]
+let [a,b] = arr
+
+ console.log(a,b)
